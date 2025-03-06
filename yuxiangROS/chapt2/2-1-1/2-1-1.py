c225fd6fd6eed22a71ceaf15da6fca51f0398770
ywbdxxm <1099245304@qq.com>
2025-03-06 10:57:58 +0800

    修改部分文件夹名称

---
+++ yuxiangROS/chapt2/2-1-1/2-1-1.py
@@ -0,0 +1,15 @@
+import rclpy
+from rclpy.node import Node
+
+
+def main():
+    rclpy.init()
+    node = Node("python_node")
+    node.get_logger().info("hello world")
+    node.get_logger().warn("hello world")
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ yuxiangROS/鱼香ROS.md
@@ -0,0 +1,14 @@
+# 鱼香 ROS2 教程
+
+## 第一章
+
+### ROS2 系统架构
+
+![alt text](img/2024-12-09-22-26-22.png)
+![alt text](img/2024-12-09-22-30-18.png)
+
+### turtlesim 实验
+
+![alt text](img/2024-12-12-19-13-19.png)
+注意这个环境变量$AMENT_PREFIX_PATH 下有没有相应的功能包和可执行文件
+![alt text](img/2024-12-12-19-49-38.png)
